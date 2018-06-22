e1626ed07e1fef5aa4f8fb1a74e210034e043293
Eric Kuo <eric@kingkit101.com>
2018-06-22 13:21:29 +0800

    Support Bit pin Control

--- BitSound-blockly.js
+++ BitSound-blockly.js
@@ -3,7 +3,7 @@
   'use strict';
 
   window.getSound = function (board, pin) {
-    return new webduino.module.getSound(board, board.getDigitalPin(pin));
+    return new webduino.module.Sound(board, board.getDigitalPin(pin));
   }
 
 }(window, window.webduino));
